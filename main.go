ec1fa15289c55eb9e1443a461aa67f9141894d26
Nick Gowdy <nickgowdy@Nicks-MacBook-Pro.local>
2022-08-17 13:52:34 +0100

    Added GET by ID

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 	"todo-list-golang/models"
 
 	"github.com/gin-gonic/gin"
@@ -10,6 +11,7 @@ import (
 func main() {
 	router := gin.Default()
 	router.GET("/todos", get)
+	router.GET("/todos/:id", getById)
 	router.POST("/todos", post)
 	router.Run("localhost:8080")
 }
@@ -17,6 +19,24 @@ func main() {
 func get(c *gin.Context) {
 	todos := models.GetTodos()
 	c.IndentedJSON(http.StatusOK, todos)
+}
+
+func getById(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "bad request"})
+	}
+
+	todos := models.GetTodos()
+	for _, t := range todos {
+		if t.Id == id {
+			c.IndentedJSON(http.StatusFound, t)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
+
 }
 
 func post(c *gin.Context) {
