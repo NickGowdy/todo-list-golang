18dff5dadda7e7148d19be3a43231e30668619e6
Nick Gowdy <nickgowdy@Nicks-MacBook-Pro.local>
2023-01-14 09:14:30 +0000

    more coverage

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -32,6 +33,42 @@ func TestInsertGetDeleteTodos(t *testing.T) {
 		t.Errorf("StatusCode should be 200 but is: %v", w.Result().StatusCode)
 	}
 
+	todo := decodeTodo(w)
+
+	if todo.Id == 0 {
+		t.Errorf("todo id should be greater than 0 but was: %v", todo.Id)
+	}
+
+	if todo.Value != "Nick's todo" {
+		t.Errorf("todo value should be: Nick's todo but was: %s", todo.Value)
+	}
+
+	if todo.IsComplete != false {
+		t.Errorf("todo value should be: false but was %v", todo.IsComplete)
+	}
+
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest(http.MethodGet, fmt.Sprintf("/todos/%v", todo.Id), nil)
+	router.ServeHTTP(w, req)
+
+	if w.Result().StatusCode != 200 {
+		t.Errorf("StatusCode should be 200 but is: %v", w.Result().StatusCode)
+	}
+
+	todo = decodeTodo(w)
+
+	if todo.Id == 0 {
+		t.Errorf("todo id should be greater than 0 but was: %v", todo.Id)
+	}
+
+	if todo.Value != "Nick's todo" {
+		t.Errorf("todo value should be: Nick's todo but was: %s", todo.Value)
+	}
+
+	if todo.IsComplete != false {
+		t.Errorf("todo value should be: false but was %v", todo.IsComplete)
+	}
+
 	w = httptest.NewRecorder()
 	req, _ = http.NewRequest(http.MethodGet, "/todos", nil)
 	router.ServeHTTP(w, req)
@@ -42,4 +79,34 @@ func TestInsertGetDeleteTodos(t *testing.T) {
 	if len(todos) == 0 {
 		t.Errorf("todos should not be empty but is: %v", len(todos))
 	}
+
+	for _, val := range todos {
+		if val.Id == 0 {
+			t.Errorf("todo id should be greater than 0 but was: %v", todo.Id)
+		}
+
+		if val.Value != "Nick's todo" {
+			t.Errorf("todo value should be: Nick's todo but was: %s", todo.Value)
+		}
+
+		if val.IsComplete != false {
+			t.Errorf("todo value should be: false but was %v", todo.IsComplete)
+		}
+	}
+
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest(http.MethodDelete, fmt.Sprintf("/todos/%v", todo.Id), nil)
+	router.ServeHTTP(w, req)
+	var truthy bool
+	json.NewDecoder(w.Body).Decode(&truthy)
+
+	if !truthy {
+		t.Errorf("truthy should be: true but was %v", truthy)
+	}
+}
+
+func decodeTodo(w *httptest.ResponseRecorder) models.Todo {
+	var todo models.Todo
+	json.NewDecoder(w.Body).Decode(&todo)
+	return todo
 }
